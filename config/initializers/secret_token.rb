34a39e902fc1bbfba3447a199b0932b0d996e77d
laransom <laransom@gmail.com>
2014-04-12 11:01:59 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EchoNest::Application.config.secret_key_base = '31d02719120d3ddcb6d773a4f6bf8c1b69d9badd9d79e2572fc19c3195621ceac3221a5969d009b9f9e3a0d3e7ca40ffd1bb5d673d961237f26f3fb956fb0315'
