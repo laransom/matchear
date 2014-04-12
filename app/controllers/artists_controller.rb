121451002fd9f1e27ba8e129964f95e905534231
laransom <laransom@gmail.com>
2014-04-12 13:45:34 -0400

    in progress

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,7 @@
 class ArtistsController < ApplicationController
 
   def index
-
+    @phish = 'http://api.jambase.com/events?artistId=2698&zipCode=95128&page=0&api_key=JGTPNR2QU4TG3XKWWMRH4UNH'
   end
 
   def show
